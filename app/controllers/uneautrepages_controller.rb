df68c60b9900d3f361816b2d3daf93b396bb3e8f
Prelang Builder <builder@prelang.com>
2014-04-13 07:49:46 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/uneautrepages_controller.rb
+++ app/controllers/uneautrepages_controller.rb
@@ -1,4 +1,8 @@
 class UneautrepagesController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_uneautrepage, only: [:show, :edit, :update, :destroy]
 
   # GET /uneautrepages
